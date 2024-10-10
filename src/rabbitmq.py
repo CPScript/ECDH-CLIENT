a1a5487f8f2cdc8ca6990803b6cb499df84145ef
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-10 02:31:47 -0400

    Update rabbitmq.py

--- src/rabbitmq.py
+++ src/rabbitmq.py
@@ -1,1 +1,10 @@
+# src/rabbitmq.py
+import pika
 
+class RabbitMQ:
+  def __init__(self):
+    self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    self.channel = self.connection.channel()
+
+  def send_message(self, username, message):
+    self.channel.basic_publish(exchange='', routing_key='chat', body=f'{username}: {message}') # Send message using RabbitMQ
