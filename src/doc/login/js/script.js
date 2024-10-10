4f43a33bc93715f33eaa2a3d03bbdd3aa9ee4247
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-10 09:17:19 -0400

    Update script.js

--- src/doc/login/js/script.js
+++ src/doc/login/js/script.js
@@ -30,19 +30,5 @@ loginButton.addEventListener('click', async () => {
 });
 
 registerButton.addEventListener('click', async () => {
-  const username = usernameInput.value;
-  const password = passwordInput.value;
-
-  const response = await fetch('/register', { // Send request to backend to register
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ username, password }),
-  });
-
-  const data = await response.json();
-  if (data.success) {
-    console.log('Registration successful');
-  } else {
-    console.error('Registration failed');
-  }
+  <a href="/src/doc/register/index.html"> // not a good way to call another script from a diffrent directory, will change later
 });
