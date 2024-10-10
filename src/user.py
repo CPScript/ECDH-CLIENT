70960386190be7fcdcdc1e296c418962b9527c8a
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-10 02:30:54 -0400

    Update user.py

--- src/user.py
+++ src/user.py
@@ -1,25 +1,17 @@
 # src/user.py
-import os
-import pickle
+import sqlite3
 
-class User:
-  def __init__(self, username, public_key, private_key):
-    self.username = username
-    self.public_key = public_key
-    self.private_key = private_key
-
-def create_user(username):
-  # Create a new user with a public-private key pair
-  public_key, private_key = generate_key_pair()
-  user = User(username, public_key, private_key)
-  with open(f'{username}.dat', 'wb') as f:
-    pickle.dump(user, f)
+def create_user(username, password):
+  conn = sqlite3.connect('users.db')   # Create user in database
+  c = conn.cursor()
+  c.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+  conn.commit()
+  conn.close()
 
 def get_user(username):
-  # Get a user's public-private key pair
-  try:
-    with open(f'{username}.dat', 'rb') as f:
-      user = pickle.load(f)
-      return user
-  except FileNotFoundError:
-    return None
+  conn = sqlite3.connect('users.db')   # Get user from database
+  c = conn.cursor()
+  c.execute('SELECT * FROM users WHERE username = ?', (username,))
+  user = c.fetchone()
+  conn.close()
+  return user
